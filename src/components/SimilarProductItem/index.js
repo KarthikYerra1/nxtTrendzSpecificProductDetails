0a1263a298f8474e5d12bc53021d0e48c64689f8
KarthikYerra1 <karthikyerra743@gmail.com>
2023-09-08 14:28:21 +0000

    Error Modified

--- src/components/SimilarProductItem/index.js
+++ src/components/SimilarProductItem/index.js
@@ -14,7 +14,7 @@ const SimilarProductItem = props => {
       <p className="similar-product-title">{title}</p>
       <p className="similar-product-brand">by {brand}</p>
       <div className="similar-product-price-rating-container">
-        <p className="similar-product-price">{price}</p>
+        <p className="similar-product-price">Rs {price}/-</p>
         <div className="similar-product-rating-container">
           <p className="similar-product-rating">{rating}</p>
           <img
